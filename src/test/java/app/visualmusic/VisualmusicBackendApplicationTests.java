20891c131410e5dc1db3e33305e4999862b6e8d5
popipopka <eg.belykh@yandex.ru>
2025-04-10 00:57:00 +0300

    chore: init spring boot project
    VM-58

---
+++ src/test/java/app/visualmusic/VisualmusicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.visualmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualmusicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
